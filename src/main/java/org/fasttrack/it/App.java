c47c8a984f39c37034951412a861b30162888a3b
Adi <popaadrianvd@gmail.com>
2019-08-07 20:14:52 +0300

    created get, update and delete methods

--- src/main/java/org/fasttrack/it/App.java
+++ src/main/java/org/fasttrack/it/App.java
@@ -1,13 +1,20 @@
 package org.fasttrack.it;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.fasttrack.it.persistance.ToDoItemRepository;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws SQLException, IOException, ClassNotFoundException {
+        ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
+        toDoItemRepository .createToDoItem("Learn Java",
+                LocalDateTime.now().plusMonths(6));
+
+        ToDoItemRepository getToDoItems = new ToDoItemRepository();
+        getToDoItems.getToDoItems();
+
     }
 }
